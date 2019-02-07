33d460fb85e9ee8ca901984bf75a259f15c0e1aa
Corbett Martin <corbtastik@gmail.com>
2019-02-07 07:58:16 -0600

    update boot and readme

--- src/main/java/io/corbs/TodosSinkApp.java
+++ src/main/java/io/corbs/TodosSinkApp.java
@@ -1,17 +1,33 @@
 package io.corbs;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.Input;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.SubscribableChannel;
 
+import java.util.Collections;
+import java.util.Set;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+class Todo {
+    private Integer id;
+    private String title = "";
+    private Boolean completed = false;
+    private Set<String> hashtags = Collections.emptySet();
+}
+
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableBinding(TodosSinkApp.SinkChannels.class)
 public class TodosSinkApp {
 
@@ -19,12 +35,16 @@ public class TodosSinkApp {
 
     interface SinkChannels {
         @Input
-        SubscribableChannel input();
+        SubscribableChannel foo();
     }
 
-    @StreamListener("input")
-    void createTodo(Todo todo) {
-        LOG.debug("TodosSinkApp createTodo invoked with todo " + todo.toString());
+    @StreamListener("foo")
+    void createTodoListener(Todo todo) {
+        LOG.info("TodosSinkApp createTodoListener handled todo " + todo.toString());
+        if(!todo.getHashtags().isEmpty()) {
+            LOG.info("Oh look a Todo with #hashtags...this must be important.");
+            LOG.info("Hashtags: " + todo.getHashtags());
+        }
     }
 
 	public static void main(String[] args) {
